83187afd645be84100f1e577382a4d61b42ad1e8
chaimaeouazzani <chaimaeouazzani20@gmail.com>
2020-10-02 23:25:39 -0400

    HelloWorld.java is now running and print Hello World and today's date

--- repo1/hello-world.java
+++ repo1/hello-world.java
@@ -1,5 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+        System.out.println("Hello, World!");
+	System.out.println("today's Date is 10/02/2020!");  
     }
 }
